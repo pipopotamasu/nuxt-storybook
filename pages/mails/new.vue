4e96fc708293f563f727466d1b9d6b737036cac8
pipopotamasu <ec0156hx@gmail.com>
2018-07-31 19:54:46 +0900

    pagesの追加

---
+++ pages/mails/new.vue
@@ -0,0 +1,13 @@
+<template>
+  <create-mails></create-mails>
+</template>
+
+<script>
+import CreateMails from '~/components/organisms/CreateMails.vue'
+
+export default {
+  components: {
+    CreateMails
+  }
+}
+</script>
