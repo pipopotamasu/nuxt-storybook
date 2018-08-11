211d8ecdd2d32e20f6291d27963ef75ad8308286
pipopotamasu <ec0156hx@gmail.com>
2018-08-11 16:38:06 +0900

    create mails

--- stories/organisms/index.js
+++ stories/organisms/index.js
@@ -2,17 +2,8 @@ import Vue from "vue"
 import Vuex from "vuex"
 import { storiesOf } from "@storybook/vue"
 import Centered from "@storybook/addon-centered"
-import {
-  withKnobs,
-  number,
-  boolean,
-  text,
-  object,
-  array
-} from "@storybook/addon-knobs"
-import { action } from "@storybook/addon-actions"
-import iconPath from "~/static/icons/wakabayashi.jpg"
 import AddFromUsers from "~/components/organisms/AddFromUsers.vue"
+import CreateMails from "~/components/organisms/CreateMails.vue"
 import { createStore } from "~/.nuxt/store"
 import "~/stories/stubs/nuxt_link"
 
@@ -20,9 +11,13 @@ Vue.use(Vuex)
 
 storiesOf("organisms", module)
   .addDecorator(Centered)
-  .addDecorator(withKnobs)
   .add("AddFromUsers", () => ({
     components: { AddFromUsers },
     template: `<add-from-users></add-from-users>`,
+    store: createStore()
+  }))
+  .add("CreateMails", () => ({
+    components: { CreateMails },
+    template: `<create-mails></create-mails>`,
     store: createStore()
   }))
