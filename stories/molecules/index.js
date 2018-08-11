68f5b97549c060f27d55e6e5eb889d02e9c042f0
pipopotamasu <ec0156hx@gmail.com>
2018-08-11 15:09:22 +0900

    MailTopics

--- stories/molecules/index.js
+++ stories/molecules/index.js
@@ -1,17 +1,30 @@
 import { storiesOf } from "@storybook/vue"
 import Centered from "@storybook/addon-centered"
-import { withKnobs, number, boolean, text } from "@storybook/addon-knobs"
+import {
+  withKnobs,
+  number,
+  boolean,
+  text,
+  object,
+  array
+} from "@storybook/addon-knobs"
 import { action } from "@storybook/addon-actions"
-import "~/stories/stubs/nuxt_link"
 import iconPath from "~/static/icons/wakabayashi.jpg"
 import FromUsers from "~/components/molecules/FromUsers.vue"
+import MailTopics from "~/components/molecules/MailTopics.vue"
+import "~/stories/stubs/nuxt_link"
 
 storiesOf("molecules", module)
   .addDecorator(Centered)
+  .addDecorator(withKnobs)
   .add("FromUsers", () => ({
     components: { FromUsers },
     template: `<from-users
-                :currentUser="{ icon_path: '${iconPath}', first_name: '太郎', last_name: '山田' }"
+                :currentUser="{
+                  icon_path: '${iconPath}',
+                  first_name: '太郎',
+                  last_name: '山田'
+                }"
                 :roomUsersCount="5"
                 :fromUsers="[
                   { id: 1, icon_path: '${iconPath}', first_name: '智', last_name: '里中' }
@@ -19,4 +32,15 @@ storiesOf("molecules", module)
                 :deleteFromUser="action"
                ></from-users>`,
     methods: { action: action("deleted!") }
+  }))
+  .add("MailTopics", () => ({
+    components: { MailTopics },
+    template: `<mail-topics
+                :topics="[
+                  { id: 1, active: false, content: 'トピック1' },
+                  { id: 2, active: true, content: 'トピック2' }
+                ]"
+                :toggle="action"
+               ></mail-topics>`,
+    methods: { action: action("toggled!") }
   }))
